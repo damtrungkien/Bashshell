4d47fdae5474504ce497bc076ba905e97a79c3e4
Đàm Trung Kiên <105965626+damtrungkien@users.noreply.github.com>
2025-05-25 14:33:51 +0700

    Update zerossl_install.sh

--- zerossl_install.sh
+++ zerossl_install.sh
@@ -1,43 +1,47 @@
 #!/bin/bash
-#DAMTRUNGKIEN
-echo "Install ZeroSSL_cPanel ( Nameserver  ns3-ns4 | va Domain da tro A, WWW, MAIL ve IP goi Host"
+#Author: DOTRUNGQUAN.INFO
+# Function to install SSL for main domain (option 1)
+install_ssl_main_domain() {
+    read -p "Nhập vào domain: " main_domain
+    wget -O - https://get.acme.sh | sh -s email=ssl@azdigi.info
+    bash /home/*/.acme.sh/acme.sh --issue --webroot ~/public_html -d $main_domain -d www.$main_domain --keylength 2048 --force
+    bash /home/*/.acme.sh/acme.sh --deploy --deploy-hook cpanel_uapi --domain $main_domain --domain www.$main_domain
+}
 
-homeuser=$(echo $PWD)
+# Function to install SSL for addon domain (option 2)
+install_ssl_addon_domain() {
+    read -p "Nhập vào domain: " addon_domain
+    wget -O - https://get.acme.sh | sh -s email=ssl@azdigi.info
+    bash /home/*/.acme.sh/acme.sh --issue --webroot ~/$addon_domain -d $addon_domain -d www.$addon_domain --keylength 2048 --force
+    bash /home/*/.acme.sh/acme.sh --deploy --deploy-hook cpanel_uapi --domain $addon_domain --domain www.$addon_domain
+}
 
-echo -n "Moi nhap email: "
-read emailkhach
+# Function to install SSL for addon domain (option 2)
+install_ssl_sub_addon_domain() {
+    read -p "Nhập vào domain: " sub_addon_domain
+    wget -O - https://get.acme.sh | sh -s email=ssl@azdigi.info
+    bash /home/*/.acme.sh/acme.sh --issue --webroot ~/$sub_addon_domain -d $sub_addon_domain --keylength 2048 --force
+    bash /home/*/.acme.sh/acme.sh --deploy --deploy-hook cpanel_uapi --domain $sub_addon_domain 
+}
 
-echo -n "Nhap Domain chinh cua Host: "
-read main_domain
+# Main script
+echo "Các tuỳ chọn:"
+echo "1. Cài đặt Main Domain"
+echo "2. Cài đặt addon Domain"
+echo "3. Cài cho sub thêm dạng Addon"
+read -p "Nhập vào (1 or 2 or 3): " option
 
-echo -n "Nhap Domain chinh de cai SSL (Neu khong cai ssl cho Domain chinh thi nhap 'KHONG'): "
-read main_domain1
-
-wget -O - https://get.acme.sh | sh -s email=$emailkhach
-
-if [ $main_domain1 != $main_domain ]
-then
-  echo "Khong cai dat ssl cho $main_domain"
-else
-  $homeuser/.acme.sh/acme.sh --issue --webroot $homeuser/public_html -d $main_domain -d www.$main_domain --force
-  sleep 3
-  $homeuser/.acme.sh/acme.sh --deploy --deploy-hook cpanel_uapi --domain $main_domain --domain www.$main_domain
-  sleep 3
-fi
-
-read -p "Nhap Domain addon de cai dat ZeroSSL: " tenmien_ssl
-
-for listaddon in $tenmien_ssl
-do
-  echo $listaddon >> $homeuser/list_addon.txt
-
-done
-
-for zerossl in `\cat $homeuser/list_addon.txt` ;
-do
-  $homeuser/.acme.sh/acme.sh --issue --webroot $homeuser/$zerossl -d $zerossl -d www.$zerossl  --force
-  $homeuser/.acme.sh/acme.sh --deploy --deploy-hook cpanel_uapi --domain $zerossl --domain www.$zerossl 
-done
-
-rm -rf $homeuser/list_addon.txt
-rm -rf $homeuser/zerossl_install.sh
+case $option in
+    1)
+        install_ssl_main_domain
+        ;;
+    2)
+        install_ssl_addon_domain
+        ;;
+    3)
+        install_ssl_sub_addon_domain
+        ;;
+    *)
+        echo "Nhập vào các tuỳ chọn 1,2 or 3"
+        ;;
+esac
